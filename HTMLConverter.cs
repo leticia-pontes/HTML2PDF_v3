9b64dc719b38c98374b0ae94f6c20850874239b7
Leticia Alves de Pontes <50830736+leticia-hub@users.noreply.github.com>
2024-10-31 23:31:32 -0300

    adicionando biblioteca

---
+++ HTMLConverter.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace HTML2PDF_v3
+{
+    public class HTMLConverter
+    {
+        public string ConvertHtmlToText(string html)
+        {
+            string formatHtml = html.Replace("<!DOCTYPE html>", "\n")
+                                .Replace("html lang=\"pt-BR\"", "\n")
+                                .Replace("<br>", "").Replace("</br>", "\n")
+                                .Replace("<hr>", "").Replace("</hr>", "\n")
+                                .Replace("<strong>", "").Replace("</strong>", "\n")
+                                .Replace("<em>", "").Replace("</em>", "\n")
+                                .Replace("<small>", "").Replace("</small>", "\n")
+                                .Replace("<mark>", "").Replace("</mark>", "\n")
+                                .Replace("<blockquote>", "").Replace("</blockquote>", "\n")
+                                .Replace("<cite>", "").Replace("</cite>", "\n")
+                                .Replace("<ul>", "").Replace("</ul>", "\n")
+                                .Replace("<ol>", "").Replace("</ol>", "\n")
+                                .Replace("<li>", "").Replace("</li>", "\n")
+                                .Replace("<a>", "").Replace("</a>", "\n")
+                                .Replace("<img>", "").Replace("</img>", "\n")
+                                .Replace("<figure>", "").Replace("</figure>", "\n")
+                                .Replace("<figcaption>", "").Replace("</figcaption>", "\n")
+                                .Replace("<table>", "").Replace("</table>", "\n")
+                                .Replace("<tr>", "").Replace("</tr>", "\n")
+                                .Replace("<td>", "").Replace("</td>", "\n")
+                                .Replace("<th>", "").Replace("</th>", "\n")
+                                .Replace("<thead>", "").Replace("</thead>", "\n")
+                                .Replace("<tbody>", "").Replace("</tbody>", "\n")
+                                .Replace("<tfoot>", "").Replace("</tfoot>", "\n")
+                                .Replace("<head>", "").Replace("</head>", "\n")
+                                .Replace("<meta charset=\"UTF-8\">", "\n")
+                                .Replace("<title>", "").Replace("</title>", "\n")
+                                .Replace("<body>", "").Replace("</body>", "\n")
+                                .Replace("<header>", "").Replace("</header>", "\n")
+                                .Replace("<h1>", "").Replace("</h1>", "\n")
+                                .Replace("<nav>", "").Replace("</nav>", "\n")
+                                .Replace("<section>", "").Replace("</section>", "\n")
+                                .Replace("<h2>", "").Replace("</h2>", "\n")
+                                .Replace("<footer>", "").Replace("</footer>", "\n")
+                                .Replace("meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"", "")
+                                .Replace("section id=\"sobre\"", "")
+                                .Replace("<a>", "").Replace("</a>", "")
+                                .Replace("&copy; 2024", "")
+                                .Replace("<p>", "").Replace("</p>", "")
+                                .Replace("<>", "").Replace("<", "").Replace(">", "")
+                                .Replace("a href=\"#sobre\"", "")
+                                .Replace("a href=\"#contato\"", "")
+                                .Replace("/html", "");
+
+            return formatHtml;
+        }
+    }
+}
---
+++ PDFGenerator.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace HTML2PDF_v3
+{
+    public class PDFGenerator
+    {
+        private HTMLConverter _htmlConverter;
+
+        public PDFGenerator()
+        {
+            _htmlConverter = new HTMLConverter();
+        }
+
+        // Adaptação para aceitar qualquer Stream
+        public void GeneratePDF(string htmlContent, Stream outputStream)
+        {
+            string pdfText = _htmlConverter.ConvertHtmlToText(htmlContent);
+
+            using (BinaryWriter writer = new BinaryWriter(outputStream, Encoding.UTF8, leaveOpen: true))
+            {
+                WritePDFHeader(writer);
+                WritePDFPage(writer, pdfText);
+                WritePDFTrailer(writer);
+            }
+
+            Console.WriteLine("PDF gerado com sucesso.");
+        }
+
+        private void WritePDFHeader(BinaryWriter writer)
+        {
+            writer.Write(Encoding.ASCII.GetBytes("%PDF-1.4\n"));
+            writer.Write(Encoding.ASCII.GetBytes("1 0 obj\n"));
+            writer.Write(Encoding.ASCII.GetBytes("<< /Type /Catalog\n"));
+            writer.Write(Encoding.ASCII.GetBytes("/Pages 2 0 R\n"));
+            writer.Write(Encoding.ASCII.GetBytes(">>\n"));
+            writer.Write(Encoding.ASCII.GetBytes("endobj\n"));
+        }
+
+        private void WritePDFPage(BinaryWriter writer, string pdfText)
+        {
+            writer.Write(Encoding.ASCII.GetBytes("2 0 obj\n"));
+            writer.Write(Encoding.ASCII.GetBytes("<< /Type /Pages\n"));
+            writer.Write(Encoding.ASCII.GetBytes("/Kids [3 0 R]\n"));
+            writer.Write(Encoding.ASCII.GetBytes("/Count 1\n"));
+            writer.Write(Encoding.ASCII.GetBytes(">>\n"));
+            writer.Write(Encoding.ASCII.GetBytes("endobj\n"));
+
+            writer.Write(Encoding.ASCII.GetBytes("3 0 obj\n"));
+            writer.Write(Encoding.ASCII.GetBytes("<< /Type /Page\n"));
+            writer.Write(Encoding.ASCII.GetBytes("/Parent 2 0 R\n"));
+            writer.Write(Encoding.ASCII.GetBytes("/MediaBox [0 0 612 792]\n"));
+            writer.Write(Encoding.ASCII.GetBytes("/Contents 4 0 R\n"));
+            writer.Write(Encoding.ASCII.GetBytes(">>\n"));
+            writer.Write(Encoding.ASCII.GetBytes("endobj\n"));
+
+            float yPosition = 700;
+            float lineHeight = 14;
+            StringBuilder contentStream = new StringBuilder();
+
+            foreach (var line in pdfText.Split('\n'))
+            {
+                if (yPosition - lineHeight < 0)
+                {
+                    yPosition = 700;
+                    contentStream.AppendLine("BT /F1 24 Tf 100 " + yPosition + " Td (Continued) Tj ET\n");
+                }
+
+                contentStream.AppendLine($"BT /F1 11 Tf 100 {yPosition} Td ({line}) Tj ET");
+                yPosition -= lineHeight;
+            }
+
+            writer.Write(Encoding.ASCII.GetBytes("4 0 obj\n"));
+            writer.Write(Encoding.ASCII.GetBytes("<< /Length " + (contentStream.Length + 15) + " >>\n"));
+            writer.Write(Encoding.ASCII.GetBytes("stream\n"));
+            writer.Write(Encoding.Latin1.GetBytes(contentStream.ToString()));
+            writer.Write(Encoding.ASCII.GetBytes("endstream\n"));
+            writer.Write(Encoding.ASCII.GetBytes("endobj\n"));
+
+            writer.Write(Encoding.ASCII.GetBytes("5 0 obj\n"));
+            writer.Write(Encoding.ASCII.GetBytes("<< /Type /Font\n"));
+            writer.Write(Encoding.ASCII.GetBytes("/Subtype /Type1\n"));
+            writer.Write(Encoding.ASCII.GetBytes("/Name /F1\n"));
+            writer.Write(Encoding.ASCII.GetBytes("/BaseFont /Helvetica\n"));
+            writer.Write(Encoding.ASCII.GetBytes(">>\n"));
+            writer.Write(Encoding.ASCII.GetBytes("endobj\n"));
+        }
+
+        private void WritePDFTrailer(BinaryWriter writer)
+        {
+            writer.Write(Encoding.ASCII.GetBytes("xref\n"));
+            writer.Write(Encoding.ASCII.GetBytes("0 6\n"));
+            writer.Write(Encoding.ASCII.GetBytes("0000000000 65535 f \n"));
+            writer.Write(Encoding.ASCII.GetBytes("0000000010 00000 n \n"));
+            writer.Write(Encoding.ASCII.GetBytes("0000000069 00000 n \n"));
+            writer.Write(Encoding.ASCII.GetBytes("0000000120 00000 n \n"));
+            writer.Write(Encoding.ASCII.GetBytes("0000000190 00000 n \n"));
+            writer.Write(Encoding.ASCII.GetBytes("0000000270 00000 n \n"));
+            writer.Write(Encoding.ASCII.GetBytes("trailer\n"));
+            writer.Write(Encoding.ASCII.GetBytes("<< /Size 6\n"));
+            writer.Write(Encoding.ASCII.GetBytes("/Root 1 0 R\n"));
+            writer.Write(Encoding.ASCII.GetBytes(">>\n"));
+            writer.Write(Encoding.ASCII.GetBytes("startxref\n"));
+            writer.Write(Encoding.ASCII.GetBytes("310\n"));
+            writer.Write(Encoding.ASCII.GetBytes("%%EOF\n"));
+        }
+    }
+}
